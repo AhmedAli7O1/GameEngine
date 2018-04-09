6ad71fb3f7e360ee137f3ce4846959316e4df72f
Ahmed Ali <contact@ahmedali.me>
2018-04-08 17:28:52 -0700

    reflectivity

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -26,6 +26,9 @@ public class MainGameLoop {
 
     RawModel model = OBJLoader.loadObjModel("dragon", loader);
     ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
+    texture.setShineDamper(100);
+    texture.setReflectivity(1);
+
     TexturedModel staticModel = new TexturedModel(model, texture);
 
     Entity entity = new Entity(staticModel, new Vector3f(0, -5, -20), 0, 0, 0, 1);
